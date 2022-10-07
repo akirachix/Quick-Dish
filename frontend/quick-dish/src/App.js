afd26ea8bc8216b508cad1d33ee69b3159c2b395
Rynah-crynah <swabrinayusufshamun@gmail.com>
2022-10-07 11:23:37 +0300

    resolved changes

--- frontend/quick-dish/src/App.js
+++ frontend/quick-dish/src/App.js
@@ -1,38 +1,23 @@
-<<<<<<< HEAD
-=======
+
 // import logo from './logo.svg';
->>>>>>> f4d2d0a919ae8e0b5566aa6cb880fd75d8ccc3aa
 import './App.css';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
-
-
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Body from "./Body"
 
 function App() {
   return (
-    <div className="App">
-      
-
-
-
-    <Router>
     <div className="app">
+      <Router>
         <Routes>
         <Route path="/" element={<Body />}>
+        <Route exact path ='/' element={<SignUp/>} component={<SignUp/>} />
+        <Route exact path ='/Login' element={<Login/>} component={<Login/>} />
         </Route>
         </Routes>
-    </div>
-    </Router>
-  
-
-
-     
-    </div>
-    
-
+        </Router>
+    </div>  
    
   );
 }
