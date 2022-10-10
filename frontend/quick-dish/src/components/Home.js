e3ee79f688d887eacb200c11b76a251980dacdb8
Rynah-crynah <swabrinayusufshamun@gmail.com>
2022-10-10 11:18:22 +0300

    styled the landing page

--- frontend/quick-dish/src/components/Home.js
+++ frontend/quick-dish/src/components/Home.js
@@ -13,8 +13,7 @@ function Home() {
         <div className="home__bodyInformation1">
            <div className='home__info'>
              <div>
-               <img className='img' src='https://thumbs.dreamstime.com/b/heart-shape-various-vegetables-fruits-healthy-food-concept-isolated-white-background-140287808.jpg' 
-               alt='sabdio'/>
+               <img className='img' src='https://thumbs.dreamstime.com/b/heart-shape-various-vegetables-fruits-healthy-food-concept-isolated-white-background-140287808.jpg'/>
              </div>
              <div className='aside__text'>
                <h3 className='veg'>Vegetables</h3>
@@ -109,8 +108,7 @@ function Home() {
           <div className='home__bodyInformation2'>
            <div className='home__info'>
              <div>
-               <img className='img' src='https://media.gettyimages.com/photos/spaghetti-bolognese-picture-id1183748291?s=612x612' 
-               alt='sabdio'/>
+               <img className='img' src='https://media.gettyimages.com/photos/spaghetti-bolognese-picture-id1183748291?s=612x612' />
              </div>
              <div className='aside__text'>
                <h3>Pasta</h3>
@@ -120,8 +118,7 @@ function Home() {
 
            <div className='home__info'>
              <div className='image'>
-               <img className='img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSak_54MVMy65nV3UwFTJplokuQXwav1MGLX0Bq2ovG&s' 
-               alt='sabdio'/>
+               <img className='img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSak_54MVMy65nV3UwFTJplokuQXwav1MGLX0Bq2ovG&s'/>
              </div>
              <div className='aside__text'>
                <h3>Grains&Cereals</h3>
@@ -131,8 +128,7 @@ function Home() {
 
            <div className='home__info'>
              <div className='image'>
-               <img className='img' src='https://media.istockphoto.com/photos/one-liter-of-fresh-milk-and-two-eggs-picture-id110898455?k=20&m=110898455&s=612x612&w=0&h=qvbNobM6MYR4SCekVeCiwGubM___imTPe_zTyHSR0SE=' 
-               alt='sabdio'/>
+               <img className='img' src='https://media.istockphoto.com/photos/one-liter-of-fresh-milk-and-two-eggs-picture-id110898455?k=20&m=110898455&s=612x612&w=0&h=qvbNobM6MYR4SCekVeCiwGubM___imTPe_zTyHSR0SE='/>
              </div>
              <div className='aside__text'>
                <h3>Dairy/Eggs</h3>
@@ -142,8 +138,7 @@ function Home() {
 
            <div className='home__info'>
              <div className='image'>
-               <img className='img' src='https://www.nutritionadvance.com/wp-content/uploads/2018/10/25-cooking-fats-and-oils.jpg' 
-               alt='sabdio'/>
+               <img className='img' src='https://www.nutritionadvance.com/wp-content/uploads/2018/10/25-cooking-fats-and-oils.jpg'/>
              </div>
              <div className='aside__text'>
                <h3>Oils&Fats</h3>
@@ -159,8 +154,7 @@ function Home() {
             <div className='home__bodyInformation3'>
            <div className='home__info3'>
              <div>
-               <img className='img3' src='https://img.sndimg.com/food/image/upload/q_92,fl_progressive,w_1200,c_scale/v1/img/recipes/31/02/58/picoYDCyf.jpg' 
-               alt='sabdio'/>
+               <img className='img3' src='https://img.sndimg.com/food/image/upload/q_92,fl_progressive,w_1200,c_scale/v1/img/recipes/31/02/58/picoYDCyf.jpg'/>
              </div>
              <div className='aside__text3'>
                <h3>Russian Fried Potatoes</h3>
@@ -173,8 +167,7 @@ function Home() {
 
            <div className='home__info3'>
              <div className='image'>
-               <img className='img3' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUeXJLqoTGbVY-kb0CrANJL2QiCdu852LA5rYwPM33Kg&s' 
-               alt='sabdio'/>
+               <img className='img3' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUeXJLqoTGbVY-kb0CrANJL2QiCdu852LA5rYwPM33Kg&s'/>
              </div>
              <div className='aside__text3'>
                <h3>Fried Mashed Potatoes</h3>
@@ -187,8 +180,7 @@ function Home() {
 
            <div className='home__info3'>
              <div className='image'>
-               <img className='img3' src='https://www.budgetbytes.com/wp-content/uploads/2022/03/Tomato-Basil-Chicken-side.jpg' 
-               alt='sabdio'/>
+               <img className='img3' src='https://www.budgetbytes.com/wp-content/uploads/2022/03/Tomato-Basil-Chicken-side.jpg'/>
              </div>
              <div className='aside__text3'>
                <h3>Tomato Basil Chicken</h3>
@@ -204,8 +196,7 @@ function Home() {
            <div className='home__bodyInformation3'>
            <div className='home__info3'>
              <div>
-               <img className='img3' src='https://img.sndimg.com/food/image/upload/q_92,fl_progressive,w_1200,c_scale/v1/img/recipes/31/02/58/picoYDCyf.jpg' 
-               alt='sabdio'/>
+               <img className='img3' src='https://img.sndimg.com/food/image/upload/q_92,fl_progressive,w_1200,c_scale/v1/img/recipes/31/02/58/picoYDCyf.jpg'/>
              </div>
              <div className='aside__text3'>
                <h3>Russian Fried Potatoes</h3>
@@ -218,8 +209,7 @@ function Home() {
 
            <div className='home__info3'>
              <div className='image'>
-               <img className='img3' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUeXJLqoTGbVY-kb0CrANJL2QiCdu852LA5rYwPM33Kg&s' 
-               alt='sabdio'/>
+               <img className='img3' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUeXJLqoTGbVY-kb0CrANJL2QiCdu852LA5rYwPM33Kg&s'/>
              </div>
              <div className='aside__text3'>
                <h3>Fried Mashed Potatoes</h3>
