a088efc8c138549565d15681baba49e197423f1e
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-11-03 14:33:20 +0300

    full recipes

--- backend/recipes_api/urls.py
+++ backend/recipes_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register_user, fetch_ingredients, fetch_categories, category_recipe, ingredient_recipe, search_recipe, fetch_favorites, add_favorite, remove_favorite
+from .views import register_user, fetch_ingredients, fetch_categories, category_recipe, ingredient_recipe, search_recipe, fetch_meal, fetch_favorites, add_favorite, remove_favorite
 
 urlpatterns = [
     path('auth/', register_user),
@@ -8,6 +8,8 @@ urlpatterns = [
     path('category/<str:meal>', category_recipe),    
     path('ingredient/<str:meal>', ingredient_recipe),
     path('search/<str:meal>', search_recipe),
+    path('meal/<str:id>', fetch_meal),
+
     path('favorites/', fetch_favorites),
     path('add-favorite/', add_favorite),
     path('remove-favorite/<int:id>', remove_favorite),
